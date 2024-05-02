88a74d248ed0b893636502c349a3b7dc9ece639c
MD SIFAT ALI <sifatali520@gmail.com>
2024-05-03 04:10:16 +0600

    Update models.py

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class human(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.PositiveIntegerField(max_length=2)
+    
+    def __str__(self):
+        return self.name
+    
